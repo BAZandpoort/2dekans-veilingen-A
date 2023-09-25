080c8c4c5ac26036e88bfc63ae799983c9188c50
nilsZP <nils.vandevelde@bazandpoort.be>
2023-09-25 10:04:59 +0200

    resolve conflicts

--- src/functions/adminFunctions.php
+++ src/functions/adminFunctions.php
@@ -1,13 +1,3 @@
 <?php 
 
-    function insertHashedPasswordIntoDatabase($connection, $id, $wachtwoord) {
-        $sql = "UPDATE tblgebruikers SET wachtwoord = '".convertPasswordToHash($wachtwoord)."' WHERE gebruikersid = '".$id."'";
-        return($connection->query($sql));
-    };
-
-    function convertPasswordToHash($wachtwoord) {
-        $hashedPassword = password_hash($wachtwoord, PASSWORD_DEFAULT);
-        return $hashedPassword;
-    };
-
 ?>
