87105e84126fedea0c95da092c33efd224b1bc03
Robi <robi.bouwens@bazandpoort.be>
2023-09-25 10:56:12 +0200

    fixed navbar

--- src/navbarlayout.php
+++ src/navbarlayout.php
@@ -19,13 +19,6 @@ include "connect.php";
       <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
     </div>
   <div class="flex-none">
-    <div>
-  <div class="form-control">
-      <?php
-        echo '<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />'; 
-      ?>
-    </div>
-  <div class="flex-none">
     <div class="dropdown dropdown-end">
       <label tabindex="0" class="btn btn-ghost btn-circle">
         <div class="indicator">
@@ -51,14 +44,7 @@ include "connect.php";
       <label tabindex="0" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
           <img src="../public/img/profile.png" />
-
     </div>
-    <div class="dropdown dropdown-end">
-      <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-
-        </div>
       </label>
       <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li>
