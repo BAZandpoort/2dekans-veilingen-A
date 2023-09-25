908b42aae964518aa3ab95b649a2629aedba6d9f
JerkoJonckersZP <jerko.jonckers@bazandpoort.be>
2023-09-25 09:29:59 +0200

    Add register page

--- src/functions/userFunctions.php
+++ src/functions/userFunctions.php
@@ -8,4 +8,12 @@ function isPasswordCorrect($connection,$password,$email){
     $resultaat = $connection->query("SELECT * FROM tblgebruikers where email = '".$email."'");
     return (password_verify($password,$resultaat->fetch_assoc()['wachtwoord']))?true:false;
 }
+function registerUser($connection, $fname, $lname, $email, $password, $profile_picture, $desc) {
+    if(empty($profile_picture)) {
+        $profile_picture = "profile.png";
+    }
+
+    $resultaat = $connection->query("INSERT INTO tblgebruikers (email, voornaam, naam, wachtwoord, profielfoto, beschrijving) VALUES ('".$email."','".$fname."','".$lname."','".$password."','".$profile_picture."','".$desc."')");
+    return $resultaat;
+}
 ?>
--- src/registreren.php
+++ src/registreren.php
@@ -1,3 +1,7 @@
+<?php
+    include "connect.php";
+    include "functions/userFunctions.php";
+?>
 <!DOCTYPE html>
 <html lang="en" class="bg-[#1D3557]">
 <head>
@@ -9,30 +13,56 @@
 </head>
 <body>
     <div class="form-control w-full max-w-md mx-auto p-3">
+        <form method="post" action="registreren.php" enctype="multipart/form-data">
         <label class="label">
             <span class="label-text text-white">Voornaam</span>
         </label>
-        <input type="text" placeholder="Voornaam" class="input input-bordered w-full max-w-md" />
+        <input type="text" id="voornaam" name="voornaam" placeholder="Voornaam" class="input input-bordered w-full max-w-md" />
         <label class="label">
             <span class="label-text text-white">Achternaam</span>
         </label>
-        <input type="text" placeholder="Achternaam" class="input input-bordered w-full max-w-md" />
+        <input type="text"  id="achternaam" name="achternaam" placeholder="Achternaam" class="input input-bordered w-full max-w-md" />
         <label class="label">
             <span class="label-text text-white">E-mailadres</span>
         </label>
-        <input type="email" placeholder="E-mailadres" class="input input-bordered w-full max-w-md" />
+        <input type="email" id="email" name="email" placeholder="E-mailadres" class="input input-bordered w-full max-w-md" />
         <label class="label">
             <span class="label-text text-white">Wachtwoord</span>
         </label>
-        <input type="password" placeholder="Wachtwoord" class="input input-bordered w-full max-w-md" />
+        <input type="password" id="wachtwoord" name="wachtwoord" placeholder="Wachtwoord" class="input input-bordered w-full max-w-md" />
         <label class="label">
         <span class="label-text text-white">Beschrijving</span>
         </label>
-        <textarea class="textarea textarea-bordered h-24" placeholder="Beschrijving"></textarea>
+        <textarea class="textarea textarea-bordered h-24 w-full max-w-md" id="beschrijving" name="beschrijving" placeholder="Beschrijving"></textarea>
+        <label class="label">
+            <span class="label-text text-white">Profielfoto</span>
+        </label>
+        <input type="file" name="file" class="file-input file-input-ghost w-full max-w-md bg-white" />
         <br>
-        <input type="file" class="file-input file-input-ghost w-full max-w-md bg-white" />
-        <br>
-        <input type="submit" id="submitknop" name="submitknop" value="Registreer" class="btn bg-white" />
+        <input type="submit" id="submitknop" name="submitknop" value="Registreer" class="btn bg-white mt-3 w-full max-w-md" />
+        </form>
     </div>
+    <?php
+        if(isset($_POST['submitknop'])) {
+            if(!(isEmailCorrect($mysqli, $_POST['email']))) {
+                $voornaam = $_POST['voornaam'];
+                $achternaam = $_POST['achternaam'];
+                $email = $_POST['email'];
+                $wachtwoord = $_POST['wachtwoord'];
+                $beschrijving = $_POST['beschrijving'];
+                $upload_dir = $_SERVER["DOCUMENT_ROOT"]."/2dekans-veilingen-A/public/img/";
+                $file_name = $_FILES['file']['name'];
+                $file_tmp = $_FILES['file']['tmp_name'];
+
+                registerUser($mysqli, $voornaam, $achternaam, $email, $wachtwoord, $file_name, $beschrijving);
+                if((empty($_POST['file']))) {
+                    move_uploaded_file($file_tmp, $upload_dir.$file_name);
+                }
+
+                /* Kan nog aangepast worden*/
+                header("Location: login.php");
+            }
+        }
+    ?>
 </body>
 </html>
