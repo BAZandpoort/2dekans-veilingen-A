2edafc1bbbf1bb346dff4e1d3422d528b64a36ff
Vic Houtmeyers <144664901+vichoutmeyerszp@users.noreply.github.com>
2023-09-29 15:53:33 +0200

    Account gegevens (#50)
    * add updateUser function
    
    * replace sql with updateUser
    
    * Replaced layout text
    
    gewoon een heleboel spullen voor CSS (opmaak)
    
    * update text center
    
    * discard Table
    
    alles wat gefixed
    
    * replace sql catching
    
    zorgen dat gegevens uit ingelogde account wordt opgehaald om aan te passen.
    
    * Update form posting
    
    * add update user
    
    * resolve SESSION
    
    * Resolve pushing content
    
    Index was stuk gegaan, is nu terug gefixed :+1:

--- src/functions/userFunctions.php
+++ src/functions/userFunctions.php
@@ -24,9 +24,36 @@ function registerUser($connection, $fname, $lname, $email, $password, $profile_p
     return $resultaat;
 }
 
+function updateUser($connection, $userid, $fname, $lname, $email, $password, $profile_picture, $desc) {
+    if(empty($profile_picture)) {
+        if(getProfilePicture($connection,$userid)){
+            $profile_picture = getProfilePicture($connection,$userid);
+        }else{
+            print $mysqli->error;
+        }
+    }
+    if(empty($password)){
+        $sql = "UPDATE tblgebruikers set email = '" . $email . "', voornaam = '" . $fname . "', naam = '" . $lname . "', profielfoto = '" . $profile_picture . "', beschrijving = '" . $desc . "' where gebruikerid = '" . $userid . "'";
+        return ($connection->query($sql));
+    }else{
+        $password = convertPasswordToHash($password);
+
+        return ($connection->query("UPDATE tblgebruikers set email = '" . $email . "', voornaam = '" . $fname . "', naam = '" . $lname . "',
+        wachtwoord = '" . $password . "', profielfoto = '" . $profile_picture . "', beschrijving = '" . $desc . "' Where gebruikerid = '" . $userid . "'"));
+    }
+}
+
 function convertPasswordToHash($password) {
     $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
     return $hashedpassword;
-};
+}
+function getUser($connection,$gebruikerid){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where gebruikerid= '".$gebruikerid."'");
+    return ($resultaat->num_rows == 0)?false:$resultaat->fetch_all(MYSQLI_ASSOC);
+}
+function getProfilePicture($connection,$userid){
+    $resultaat = $connection->query("SELECT * FROM tblgebruikers where gebruikerid= '".$userid."'");
+    return ($resultaat->num_rows == 0)?false:$resultaat->fetch_assoc()['profielfoto'];
+}
 
 ?>
