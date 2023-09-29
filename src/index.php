33ea47751c1275eb4057e309b5e4506d2e6f8f73
JurnGG <130973454+JurnGG@users.noreply.github.com>
2023-09-29 16:22:18 +0200

    fix navbar search

--- src/index.php
+++ src/index.php
@@ -13,13 +13,47 @@ session_start();
 </head>
 
 <body>
-  <div class="navbar bg-base-100">
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+  <a class="btn btn-ghost normal-case text-xl">2dekans veilingen</a>
+  </div>
+  <div class="navbar-center">
+    <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto " />
+  </div>
+  <div class="navbar-end">
+    <div class="dropdown dropdown-end ">
+      <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+          <div class="card-body">
+            <span class="font-bold text-lg">8 Items</span>
+            <span class="text-info">Subtotal: $999</span>
+            <div class="card-actions">
+              <button class="btn btn-primary btn-block">View cart</button>
+      </div>
+    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img src="../public/img/profile.png" />
+          </div>
+        </label>
+        <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <li>
+            <a class="justify-between">
+              Profile
+              <span class="badge">New</span>
+            </a>
+          </li>
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
+  </div>
+</div>
+ <!--<div class="navbar bg-base-100">
     <div class="flex-1">
       <a class="btn btn-ghost normal-case text-xl">2dekans veilingen</a>
     </div>
     <div class="flex">
       <div class="form-control">
-        <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+        <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto " />
       </div>
       <div class="flex-none">
         <div class="dropdown dropdown-end">
@@ -75,5 +109,5 @@ session_start();
   </div>
   </div>
 </body>
-
+        -->
 </html>
