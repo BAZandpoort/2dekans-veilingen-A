c5fe3e94a779ed16862efe79d6974a4f23a1448d
Vic Houtmeyers <vic.houtmeyers@bazandpoort.be>
2023-10-11 16:13:22 +0200

    Add error + succes messages

--- src/overzichtVeilingen.php
+++ src/overzichtVeilingen.php
@@ -14,6 +14,37 @@
     include "functions/adminFunctions.php";
     session_start();
 
+    if(isset($_GET["error1"])){
+        $_SESSION["productid"] = "empty";
+        print'<div class="alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>Error! Failed to succesfully bid, try logging out and in again.</span>
+    </div>';
+    }
+    if(isset($_GET["error2"])){
+        $_SESSION["productid"] = "empty";
+        print'<div class="alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>Error! Can not bid under minimal price.</span>
+    </div>';
+    }
+
+    if(isset($_GET["error3"])){
+        $_SESSION["productid"] = "empty";
+        print'<div class="alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>Error! No product selected.</span>
+    </div>';
+    }
+
+    if (isset($_GET["succes"])) {
+        $_SESSION["productid"] = "empty";
+        print  '<div class="alert alert-success">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                  <span>Your bid has been added, good luck!</span>
+                </div>';
+    }
+
     $sql = "SELECT* FROM tblproducten";
     $resultaat = $mysqli->query($sql);
 
