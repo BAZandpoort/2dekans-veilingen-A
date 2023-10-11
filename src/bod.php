0434d09082e5924bab4695cf32a0d7359013024a
Vic Houtmeyers <vic.houtmeyers@bazandpoort.be>
2023-10-11 23:35:59 +0200

    Add basic layout
    Heb niet echt enorme open oog voor layout, dus als het mogelijk is, geef dan liefst wat instructies hoe je het wilt of pas het zelf eventjes toe

--- src/bod.php
+++ src/bod.php
@@ -37,7 +37,7 @@ $sql = "INSERT INTO tblboden (productid, bod, gebruikersid) VALUES ('". $_SESSIO
           header("location: overzichtVeilingen.php?error1");
         }
         $mysqli->close();
-
+// justify-center
      
 }
 }
@@ -56,16 +56,27 @@ if (isset($_GET["product"])) {
     <script src="https://cdn.tailwindcss.com"></script>
     <title>title</title></head>
 <body>
-<div class="form-control h-full flex items-center justify-center">
+<div class="form-control flex items-center bg-[#F1FAEE]">
   <label class="label">
-    <span class="label-text text-2xl">Geef uw bod in</span>
+    <span class="label-text text-2xl">Add your bid</span>
   </label>
   <form method="post" action="bod.php" >
-  <label class="input-group text-2xl">
+  <label class="input-group">
     <input type="number" placeholder="0.00" class="input input-bordered" name="bod"/>
     <button type="submit" class="btn btn-warning" name="bied" >Bid</button>
 </form>
   </label>
 </div>
 
+<div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-200">
+  <div class="collapse-title text-xl font-medium">
+    Please be aware of following guidelines and or tips:
+  </div>
+  <div class="collapse-content"> 
+    <p>1. You must bid above the asked minimum, or your bid will not be accepted.</p>
+    <p>2. Please know that there are others bidding for your wished item as well.</p>
+    <p>3. Stay alert of the timer of the item you are bidding for. This is like a competition after all to win the item.</p>
+    <p>4. You will receive a notification if you did have the highest bid so you can communicate with the seller to buy the product.</p>
+  </div>
+</div>
 </body>
