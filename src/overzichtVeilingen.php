8eb45713ce86fe421f938bb10e9a991568ca6ea0
Vic Houtmeyers <vic.houtmeyers@bazandpoort.be>
2023-10-12 17:37:10 +0200

    Moved error2 to bod.php

--- src/overzichtVeilingen.php
+++ src/overzichtVeilingen.php
@@ -21,13 +21,6 @@
         <span>Error! Failed to succesfully bid, try logging out and in again.</span>
     </div>';
     }
-    if(isset($_GET["error2"])){
-        $_SESSION["productid"] = "empty";
-        print'<div class="alert alert-error">
-        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <span>Error! Can not bid under minimal price.</span>
-    </div>';
-    }
 
     if(isset($_GET["error3"])){
         $_SESSION["productid"] = "empty";
