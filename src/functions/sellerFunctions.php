bcf7f2eda19a9ba125da66a19794de67005efc0f
abdelilahBZ <68385134+AbdelilahBZ@users.noreply.github.com>
2023-10-13 13:53:03 +0200

    Product details (#72)
    * add productDetails.php
    
    * Update productDetails.php
    
    * fix image not loading
    
    * Update productDetails.php
    
    * Update productDetails.php
    
    * Update productDetails.php
    
    * add better layout
    
    ---------
    
    Co-authored-by: nilsZP <nils.vandevelde@bazandpoort.be>

--- src/functions/sellerFunctions.php
+++ src/functions/sellerFunctions.php
@@ -3,4 +3,21 @@ function addProduct($connection, $userid,$naam, $beschrijving, $prijs, $categori
     return($connection ->query("INSERT INTO tblproducten (verkoperid,naam, beschrijving, prijs, categorie, foto, eindtijd ) VALUES ($userid,'".$naam."'
         , '".$beschrijving."','" .$prijs."','" .$categorie."','" .$foto."','".$eindtijd."')"));
 }
+
+function getProductInfo($connection, $productID) {
+    return ($connection->query("SELECT * FROM tblproducten WHERE productid = '".$productID."'"));
+};
+
+function getSeller($connection, $sellerID) {
+    return ($connection->query("SELECT * FROM tblgebruikers WHERE gebruikerid = '".$sellerID."'")); 
+};
+
+function getSellerName($connection, $sellerID) {
+    return getSeller($connection, $sellerID)->fetch_assoc()['voornaam'];
+};
+
+function getSellerLastName($connection, $sellerID) {
+    return getSeller($connection, $sellerID)->fetch_assoc()['naam'];
+};
+
 ?>
--- src/producten.php
+++ src/producten.php
@@ -21,16 +21,18 @@ include "components/navbar.php";
       $categorie = $_GET['gekozenCategorie'];
       foreach(getGekozenCategorie($mysqli, $categorie) as $row) {
         echo '
-        <div class="card card-compact w-96 bg-white shadow-lg float-right">
-          <figure><img id="productFoto" src="../public/img/'.$row['foto'].'" alt="'.$row['foto'].'"/></figure>
-            <div class="card-body">
-              <h2 id="productNaam" class="card-title">'.$row['naam'].'</h2>
-              <p id="productPrijs"></p>
-              <p id="productBeschrijving">'.$row['beschrijving'].'</p>
-              <div id="aankoop" class="card-actions justify-end">
-                <button id="aankoopKnop" class="btn btn-primary">Buy Now</button>
-              </div>
-            </div>
+        <div class="card card-compact w-96 bg-base-100 shadow-xl float-right">
+            <figure>
+                <img id="productFoto" src="../public/img/'.$row['foto'].'" alt="'.$row['foto'].'"/></figure>
+                <div class="card-body">
+                    <a href="productDetails.php?gekozenProduct='.$row['productid'].'" id="productNaam" class="card-title">'.$row['naam'].'</a>
+                    <p id="productPrijs">€'.$row['prijs'].'</p>
+                    <p id="productBeschrijving">'.$row['beschrijving'].'</p>
+                    <div id="aankoop" class="card-actions justify-end">
+                        <button id="aankoopKnop" class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </figure>
         </div>
         <div class="divider divider-horizontal"></div>
         ';
