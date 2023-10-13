0abd29229a6523c3ddcf8d66dd7f30f59e1ca3f7
abdelilahelhachiouiBZ <abdelilah.elhachioui@bazandpoort.be>
2023-10-13 11:38:12 +0200

    Update productDetails.php

--- src/productDetails.php
+++ src/productDetails.php
@@ -17,16 +17,21 @@ if (isset($_GET['gekozenProduct'])) {
     foreach(getProductInfo($mysqli, $_GET['gekozenProduct']) as $row) {
       echo '
       <div class="divider"></div>
-      <div class="card card-side bg-base-25 shadow-xl">
-        <figure class="w-3/5">
+      <div class="bg-[#F1FAEE] card card-side bg-base-100 shadow-xl">
+        <figure class="flex w-3/5">
           <img id="productFoto" class="float-right items-center w-3/5" src="../public/img/'.$row['foto'].'" alt="'.$row['foto'].'"/>
         </figure>
-          <div class="card-body">
+        <div class="divider divider-horizontal"></div>
+        <div class="card-body">
+          <div class="card bg-base-50 bg-[#FFFFFF]">
+            <div class="card-body">
               <p id="productNaam"> - Product naam: '.$row['naam'].'</p>
               <p id="productPrijs"> - Product prijs: €'.$row['prijs'].'</p>
               <p id="productBeschrijving"> - Product beschrijving: '.$row['beschrijving'].'</p>
               <p id="productVerkoper"> - Verkoper van het product: '.getSellerName($mysqli, $row['verkoperid']).' '.getSellerLastName($mysqli, $row['verkoperid']).'</p>
+            </div>
           </div>
+        </div>
       </div>
       ';
     };
