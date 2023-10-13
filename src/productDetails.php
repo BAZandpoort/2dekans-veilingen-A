3137317ed4b0f474b84b065d47baac01bd7ac049
nilsZP <nils.vandevelde@bazandpoort.be>
2023-10-13 13:49:59 +0200

    add better layout

--- src/productDetails.php
+++ src/productDetails.php
@@ -11,24 +11,24 @@ include "./functions/sellerFunctions.php";
     <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.4/dist/full.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-[#F1FAEE]">
 <?php
 if (isset($_GET['gekozenProduct'])) {
     foreach(getProductInfo($mysqli, $_GET['gekozenProduct']) as $row) {
       echo '
       <div class="divider"></div>
-      <div class="bg-[#F1FAEE] card card-side bg-base-100 shadow-xl">
-        <figure class="flex w-3/5">
-          <img id="productFoto" class="float-right items-center w-3/5" src="../public/img/'.$row['foto'].'" alt="'.$row['foto'].'"/>
+      <div class="bg-[#F1FAEE] card card-side bg-base-100 shadow-xl border-2">
+        <figure class="flex w-2/5 object-scale-down">
+          <img id="productFoto" class="float-right items-center" src="../public/img/'.$row['foto'].'" alt="'.$row['foto'].'"/>
         </figure>
         <div class="divider divider-horizontal"></div>
         <div class="card-body">
           <div class="card bg-base-50 bg-[#FFFFFF]">
             <div class="card-body">
-              <p id="productNaam"> - Product naam: '.$row['naam'].'</p>
-              <p id="productPrijs"> - Product prijs: €'.$row['prijs'].'</p>
-              <p id="productBeschrijving"> - Product beschrijving: '.$row['beschrijving'].'</p>
-              <p id="productVerkoper"> - Verkoper van het product: '.getSellerName($mysqli, $row['verkoperid']).' '.getSellerLastName($mysqli, $row['verkoperid']).'</p>
+              <h2 class="text-2xl font-bold" id="productNaam"> '.$row['naam'].'</h2>
+              <p class="text-slate-400" id="productVerkoper">'.getSellerName($mysqli, $row['verkoperid']).' '.getSellerLastName($mysqli, $row['verkoperid']).'</p>
+              <div id="productprijs" class="badge badge-outline text-black"> € '.$row["prijs"].'</div> 
+              <p id="productBeschrijving">'.$row['beschrijving'].'</p>
             </div>
           </div>
         </div>
