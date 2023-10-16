5a55df4af74ee1841c2a78535baf8b672fd93e0c
abdelilahelhachiouiBZ <abdelilah.elhachioui@bazandpoort.be>
2023-10-16 15:26:53 +0200

    add memcached lib to folders

--- src/functions/adminFunctions.php
+++ src/functions/adminFunctions.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+
+require('../memcached/memcached-api.php');
+
 function getTimeDifference($endTime) {
     $currentDate = date("Y-m-d H:i:s");
     $endDate = strtotime($endTime);
@@ -7,4 +10,37 @@ function getTimeDifference($endTime) {
     $timeDifference = date("H:i:s", $timeDifference);
     return $timeDifference;
 }
+
+function cache_start($port) {
+    $cacheObject = new Memcached();
+
+    if (!$cacheObject) {
+        print("Problems starting the caching system!");
+    } else {
+        $cacheObject->addServer('localhost', $port);
+    };
+
+    return $cacheObject;
+};
+
+function cache_createKey() {
+
+};
+
+function cache_getCacheObject($cacheObject) {
+    return $cacheObject;
+};
+
+function cache_get($cacheID) { 
+
+};
+
+function cache_update() {
+
+}
+
+function cache_stop($cacheObject) {
+    $cacheObject->quit();
+};
+
 ?>
