cc7c8de32d650bce5a102dbf40307eeac7e0d9ba
Robi <robi.bouwens@bazandpoort.be>
2023-10-20 15:37:58 +0200

    Update chatSystem.php

--- src/chatSystem.php
+++ src/chatSystem.php
@@ -2,9 +2,29 @@
 <html>
 <head>
     <meta charset="UTF-8" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.7.4/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>title</title>
 </head>
-<body>
+<body class="min-h-screen bg-[#F1FAEE]">
+    <?php
+     include "connect.php"; 
+     include "components/navbar.php";
+    ?>
+    <?php
     
+    echo '
+<div class="chat chat-start">
+  <div class="chat-bubble">Welcome to our live chat.</div>
+  <div class="chat-footer opacity-50">
+  </div>
+</div>
+<div class="chat chat-end">
+  <div class="chat-bubble">Hi I have a question</div>
+  <div class="chat-footer opacity-50">
+  </div>
+</div>
+<textarea class="textarea textarea-bordered" placeholder="write here your message"></textarea>'; 
+?>
 </body>
 </html>
