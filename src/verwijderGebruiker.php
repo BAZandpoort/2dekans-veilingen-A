f39d1ab035e143740fdb6b96627fe2c2ca4cd746
nilsZP <nils.vandevelde@bazandpoort.be>
2023-10-20 16:14:13 +0200

    update header

--- src/verwijderGebruiker.php
+++ src/verwijderGebruiker.php
@@ -3,7 +3,7 @@ include "connect.php";
 include "functions/adminFunctions.php";
 echo "<h1>Record verwijderen</h1>";
     if (deleteUser($mysqli, $_GET["verwijder"]) && (deleteProducts($mysqli, $_GET["verwijder"]))) {
-       // header("location: index.php");
+       header("location: index.php");
     } else {
         echo "Record niet kunnen verwijderen: " . $mysqli->error;
     }
