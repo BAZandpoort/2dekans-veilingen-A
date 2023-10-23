12529978fbed7866dd51c3728fb7a503430ef34e
CasperZP <casper.nauwelaerts@bazandpoort.be>
2023-10-23 10:06:19 +0200

    add themes

---
+++ src/themes.js
@@ -0,0 +1,22 @@
+module.exports = {
+  
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+      
+            "primary": "#f3f4f6",        
+            "secondary": "#bf3e1e",               
+            "accent": "#f276d7",               
+            "neutral": "#111827",               
+            "base-100": "#1f2937",                
+            "info": "#506ae2",                
+            "success": "#14673f",
+            "warning": "#cd820a",            
+            "error": "#f87059",
+          },
+        },
+        "dark",
+      ],
+    },
+  }
