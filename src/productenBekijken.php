0b6477be5de7f995c181950850b972d1945d177e
MohamedZP <144694766+MohamedZP@users.noreply.github.com>
2023-11-10 14:03:03 +0100

    UpdateproductBekijken

--- src/productenBekijken.php
+++ src/productenBekijken.php
@@ -15,10 +15,10 @@
 	require 'lang.php';
 //session_start();
 
-/*if(!isset($_SESSION['login'])){
+if(!isset($_SESSION['login'])){
   header('location: index.php');
   return;
-};*/
+};
 	echo '<h1 align="center">Producten bekijken<h1>';
 	//Als er geen gekozenverkoper is dan moet het naar index.
   if (!isset($_GET['gekozenVerkoper'])) {
@@ -38,7 +38,7 @@ echo '
   foreach(getSellerProducts($mysqli, $sellerID) as $row){
   	  	echo '
   	  	<div class="mr-4 mt-11 w-80  overflow-hidden rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300 hover:scale-105 hover:shadow-lg">
-  <img id = "productFoto" class="h-48 w-full object-cover object-center" src="../public/img/'.$row['foto'].'" alt="'.$row['foto'].'" />
+  <a href = "productDetails.php?gekozenProduct='.$row['productid'].'"><img id = "productFoto" class="h-48 w-full object-cover object-center" src="../public/img/'.$row['foto'].'" alt="'.$row['foto'].'" /></a> 
   <div class="p-4">
     <h2 class="mb-2 text-lg font-medium dark:text-white text-gray-900">'.$row['naam']. '</h2>
     <p class="mb-2 text-base dark:text-gray-300 text-gray-700">'.$row['beschrijving']. '</p>
@@ -46,6 +46,7 @@ echo '
       <p class="mr-2 text-lg font-semibold text-gray-900 dark:text-white">€ '.$row['prijs']. '</p>
     </div>
      <p class="mb-2 text-base dark:text-gray-300 text-gray-700">'.$row['categorie'].'</p>
+     
   </div>
 </div>';
   };
