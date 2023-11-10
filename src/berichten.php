5db349eac33bedc221569ee5670653589950f587
Robi <robi.bouwens@bazandpoort.be>
2023-11-10 15:35:45 +0100

    Update berichten.php

--- src/berichten.php
+++ src/berichten.php
@@ -17,6 +17,16 @@ include "functions/chatFunctions.php";
         $data = getnotification($mysqli, $user);
         foreach ($data as $value) {
             echo'<a href="'.$value["link"].'">'.$value["notificatie"].'</a>'; 
+        
+       echo ' <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+        <h2 class="card-title">'.$value["notificatie"].'</h2>
+         <p>If a dog chews shoes whose shoes does he choose?</p>
+         <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+          </div>
+         </div>
+        </div>' ;
         }
     ?>
 </body>
