31f3ce01fb4eb467c80f69313e7df28c2317b249
Robi <robi.bouwens@bazandpoort.be>
2023-11-17 14:44:04 +0100

    update chatfunctions.php
    changed every function into a prepare statement

--- src/functions/chatFunctions.php
+++ src/functions/chatFunctions.php
@@ -1,49 +1,57 @@
 <?php
 include "../src/connect.php";
-function getChatData($mysqli, $chatid){
-    $resultaat = $mysqli->query("SELECT * FROM tblchat WHERE gesprekID='".$chatid."'"); 
+include "../components/util.php";
+function getChatData($chatid){
+    $resultaat = fetch("SELECT * FROM tblchat WHERE gesprekID= ?",['type' => 'i', 'values' => $chatid]); 
     return ($resultaat->num_rows == 0)?false:$resultaat->fetch_all(MYSQLI_ASSOC); 
 }
 
-function InsertIntoChatTbl($mysqli, $ontvanger, $zenderVoornaam, $zenderAchternaam, $bericht, $chatid){
-$sql = "INSERT INTO tblchat (gesprekID, ontvanger, zenderVoornaam, zenderAchternaam, bericht) VALUES ('".$chatid."','".$ontvanger."','".$zenderVoornaam."','".$zenderAchternaam ."','".$bericht."')";
-return $mysqli->query($sql);
+function InsertIntoChatTbl($ontvanger, $zenderVoornaam, $zenderAchternaam, $bericht, $chatid){
+$resultaat = insert("INSERT INTO tblchat (gesprekID, ontvanger, zenderVoornaam, zenderAchternaam, bericht) VALUES ('?','?','?','?','?')",
+    ['type' => 'i', 'value' => $chatid],
+    ['type' => 's', 'value' => $ontvanger],
+    ['type' => 's', 'value' => $zenderVoornaam], 
+    ['type' => 's', 'value' => $zenderAchternaam], 
+    ['type' => 's', 'value' => $bericht]
+);
+return $resultaat;
  
 }
 
-function getZender ($mysqli, $gebruikersid) {
- $resultaat = $mysqli -> query ("SELECT * FROM tblgebruikers WHERE gebruikerid =".$gebruikersid); 
+function getZender ($gebruikersid) {
+ $resultaat = fetch("SELECT * FROM tblgebruikers WHERE gebruikerid = ?",['type' => 'i', 'value' => $gebruikersid]); 
  return ($resultaat ->num_rows == 0)?false:$resultaat->fetch_all(MYSQLI_ASSOC); 
 }
 
-function getOntvanger ($mysqli, $user) {
-    $resultaat = $mysqli->query("SELECT * FROM tblgebruikers WHERE gebruikerid = ".$user);
+function getOntvanger ($user) {
+    $resultaat = fetch("SELECT * FROM tblgebruikers WHERE gebruikerid = ?",['type' => 'i', 'value' => $user]);
     return ($resultaat->num_rows == 0)?false:$resultaat->fetch_assoc()['voornaam']; 
 }
 
-function getnotification ($mysqli, $user) {
-    $resultaat = $mysqli ->query("SELECT* FROM tblnotifications WHERE ontvangersid=".$user." ORDER BY id DESC"); 
+function getnotification ($user) {
+    $resultaat = fetch("SELECT * FROM tblnotifications WHERE ontvangersid= ? ORDER BY id DESC",['type' =>'i', 'value' => $user]); 
     return ($resultaat->num_rows == 0)?false:$resultaat->fetch_all(MYSQLI_ASSOC); 
 }
 
-function updateNotification ($mysqli, $id) {
-    $sql = ("UPDATE tblnotifications
-    SET status = '1'
-    WHERE id = ".$id); 
-    return $mysqli -> query($sql); 
+function updateNotification ($id) {
+    $resultaat = insert("UPDATE tblnotifications SET status = '1' WHERE id = ?",['type' => 'i', 'value' => $id]); 
+    return $resultaat; 
 }
 
-function createNotification ($mysqli, $ontvangersid, $link) {
-    $sql = ("INSERT INTO tblnotifications (notificatie, ontvangersid, status, link)
-    VALUES ('Je hebt een nieuw bericht'," . $ontvangersid . ",0, '".$link."')") ;
-    return $mysqli -> query($sql);
+function createNotification ($ontvangersid, $link) {
+    $resultaat = insert("INSERT INTO tblnotifications (notificatie, ontvangersid, status, link)
+    VALUES ('Je hebt een nieuw bericht',?,0,?)",
+    ['type' => 'i', 'value' => $ontvangersid], 
+    ['type' => 's', 'value' => $link]
+    ) ;
+    return $resultaat;
 }
-function deleteNotification($mysqli, $id) {
-    $sql = ("DELETE FROM tblnotifications WHERE id = " .$id); 
-    return $mysqli -> query($sql); 
+function deleteNotification($id) {
+    $resultaat = insert("DELETE FROM tblnotifications WHERE id = ?",['type' => 'i', 'value' => $id]); 
+    return $resultaat; 
 }
-function delectechat($mysqli, $chatid) {
-    $sql = ("DELETE FROM tblnotifications WHERE id= '" .$chatid."'"); 
-    return $mysqli -> query($sql); 
+function delectechat($chatid) {
+    $resultaat = insert("DELETE FROM tblnotifications WHERE id=?",['type' => 'i', 'value' => $chatid]); 
+    return $resultaat; 
 }
 ?>
