9c98b500cf11900799d2e8f56800e7311ac30985
nilsZP <nils.vandevelde@bazandpoort.be>
2023-11-24 14:06:25 +0100

    update database

--- public/database/db2dekansveilingen.sql
+++ public/database/db2dekansveilingen.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 16 nov 2023 om 20:21
+-- Gegenereerd op: 24 nov 2023 om 14:03
 -- Serverversie: 10.4.28-MariaDB
 -- PHP-versie: 8.2.4
 
@@ -28,11 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tblboden` (
-  `bodenId` int(11) NOT NULL,
   `productid` int(11) NOT NULL,
   `bod` decimal(10,2) NOT NULL,
   `gebruikersid` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -40,36 +39,12 @@ CREATE TABLE `tblboden` (
 -- Tabelstructuur voor tabel `tblcache`
 --
 
-INSERT INTO `tblboden` (`bodenId`, `productid`, `bod`, `gebruikersid`) VALUES
-(1, 1, 20.00, 2),
-(2, 1, 21.50, 2),
-(3, 1, 22.40, 2),
-(4, 1, 30.67, 2),
-(5, 1, 31.80, 2),
-(6, 1, 32.00, 3),
-(7, 1, 33.00, 3),
-(8, 1, 34.00, 3),
-(9, 1, 35.00, 3),
-(21, 1, 52.00, 4),
-(11, 1, 36.00, 3),
-(12, 1, 37.00, 2),
-(17, 1, 46.00, 2),
-(14, 1, 39.00, 4),
-(15, 1, 40.00, 2),
-(16, 1, 45.00, 2),
-(18, 1, 47.00, 2),
-(19, 1, 50.00, 4),
-(20, 1, 51.00, 4),
-(22, 1, 67788.00, 4),
-(23, 1, 7000000.00, 3),
-(24, 3, 2.00, 3),
-(25, 3, 3.00, 2);
 CREATE TABLE `tblcache` (
   `cacheid` int(11) NOT NULL,
   `gebruikerid` int(11) NOT NULL,
   `cachenaam` varchar(255) NOT NULL,
   `cachewaarde` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -79,15 +54,34 @@ CREATE TABLE `tblcache` (
 
 CREATE TABLE `tblcategorieen` (
   `categorienaam` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tblcategorieen`
 --
 
 INSERT INTO `tblcategorieen` (`categorienaam`) VALUES
-('elektronica'),
-('speelgoed');
+('Sap');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `tblchat`
+--
+
+CREATE TABLE `tblchat` (
+  `gesprekid` text NOT NULL,
+  `ontvangerid` text NOT NULL,
+  `zenderid` int(11) NOT NULL,
+  `link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `tblchat`
+--
+
+INSERT INTO `tblchat` (`gesprekid`, `ontvangerid`, `zenderid`, `link`) VALUES
+('edff554a7101d1f11bfc37ecee460ce6', '8', 7, 'chatSystem.php?user=8&chatid=edff554a7101d1f11bfc37ecee460ce6');
 
 -- --------------------------------------------------------
 
@@ -101,7 +95,7 @@ CREATE TABLE `tblfacturen` (
   `koperid` int(11) NOT NULL,
   `datum` timestamp NOT NULL DEFAULT current_timestamp(),
   `factuurpdf` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -112,7 +106,7 @@ CREATE TABLE `tblfacturen` (
 CREATE TABLE `tblfavorieten` (
   `productid` int(11) NOT NULL,
   `gebruikerid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -130,6 +124,7 @@ CREATE TABLE `tblgebruikers` (
   `status` text NOT NULL,
   `profielfoto` text NOT NULL,
   `beschrijving` text NOT NULL,
+  `adres` text NOT NULL,
   `theme` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -137,11 +132,38 @@ CREATE TABLE `tblgebruikers` (
 -- Gegevens worden geëxporteerd voor tabel `tblgebruikers`
 --
 
-INSERT INTO `tblgebruikers` (`gebruikerid`, `email`, `voornaam`, `naam`, `wachtwoord`, `admin`, `status`, `profielfoto`, `beschrijving`, `theme`) VALUES
-(1, 'jurn@gmail.com', 'jurn', 'dd', '$2y$10$5OywTtSKA8vNv3pX/rX9.eYDRMIuu2xyfHZcTAebkxf/IXeW2W2la', 0, '', 'monkey.jpg', '', ''),
-(2, 'test@gmail.com', 'test', 'dd', '$2y$10$6kylllePP7cds53wXeDfguP0V/uBDumcTZoUFcqYPz1Io2173U75u', 0, '', 'profile.png', 'dd', 'dark'),
-(3, 'casper.nauwelaerts@gmail.com', 'Casper', 'Nauwelaerts', '$2y$10$6aHJLkvvreVnKfS4676qb.ZleoPjlTU1G5Q4IxiiLo.BYFVyf4UDi', 0, '', 'profile.png', 'xdfhxd', 'retro'),
-(4, 'casper@bazandpoort.be', 'Casper', 'Nauwelaerts', '$2y$10$6bLlGEUgnN5xJ/KgBN5ujeqkw3PqUYCRuds0IA6JnzQ.Lc5fZPmK2', 0, '', 'profile.png', 'tycjctcyt', '');
+INSERT INTO `tblgebruikers` (`gebruikerid`, `email`, `voornaam`, `naam`, `wachtwoord`, `admin`, `status`, `profielfoto`, `beschrijving`, `adres`, `theme`) VALUES
+(7, 'nils@gmail.com', 'Nils', 'Van de Velde', '$2y$10$AbPFa5UMFAk3INfdc3zXO.e5J.aelt5o9Vq8w.rhS3yfmpT1HwoO2', 0, '', 'RVIM5HMA_400x4001.jpg', 'admin acc', '', ''),
+(8, 'de@de.de', 'de', 'de', '$2y$10$7xic70cKqsb6vISj1V82j.BkVQDwqtf/FL.3aR3wkaIx1D.SYJNeu', 0, '', 'art1.png', 'de verkoper', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `tblmessage`
+--
+
+CREATE TABLE `tblmessage` (
+  `messageid` int(11) NOT NULL,
+  `chatid` int(11) NOT NULL,
+  `zenderid` int(11) NOT NULL,
+  `ontvangerid` int(11) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `tblmessage`
+--
+
+INSERT INTO `tblmessage` (`messageid`, `chatid`, `zenderid`, `ontvangerid`, `message`) VALUES
+(1, 0, 7, 8, 'hallo meneer de verkoper'),
+(2, 0, 7, 8, 'ik zou graag uw sap hebben voor 1.80 euro'),
+(3, 0, 8, 8, 'de minimum prijs is 2 euro meneer'),
+(4, 0, 8, 8, 'heeft u dyslexie???'),
+(5, 0, 7, 8, 'dssf'),
+(6, 0, 7, 8, 'kut kop'),
+(7, 0, 7, 8, 'gierighaard\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nµ\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n'),
+(8, 0, 7, 8, 'gierig\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\ns'),
+(9, 0, 7, 8, 'd\r\n\r\nd');
 
 -- --------------------------------------------------------
 
@@ -159,16 +181,14 @@ CREATE TABLE `tblproducten` (
   `categorie` text NOT NULL,
   `startdatum` timestamp NOT NULL DEFAULT current_timestamp(),
   `eindtijd` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tblproducten`
 --
 
 INSERT INTO `tblproducten` (`productid`, `verkoperid`, `foto`, `naam`, `prijs`, `beschrijving`, `categorie`, `startdatum`, `eindtijd`) VALUES
-(1, 1, 'monkey.jpg', 'grasmaaier', '22', 'Dit is een grasmaaier.', 'speelgoed', '2023-10-06 13:45:32', '0000-00-00 00:00:00'),
-(2, 1, 'Appelsap_voedingswaarde.jpg', 'gebouwhuis\r\n', '22', 'Dit is een grasmaaier.', 'speelgoed', '2023-10-06 13:45:32', '0000-00-00 00:00:00'),
-(3, 1, 'RVIM5HMA_400x400.jpg', 'grasmaaier', '22', 'Dit is een grasmaaier.', 'elektronica\r\n', '2023-10-06 13:45:32', '0000-00-00 00:00:00');
+(10, 8, 'Appelsap_voedingswaarde1.jpg', 'sap', 2, 'lekker sappig sap van sap', 'Sap', '2023-11-24 08:40:48', '2023-11-24 14:40:48');
 
 -- --------------------------------------------------------
 
@@ -182,17 +202,13 @@ CREATE TABLE `tblrapporten` (
   `melderid` int(11) NOT NULL,
   `reden` text NOT NULL,
   `behandeld` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexen voor tabel `tblboden`
---
-ALTER TABLE `tblboden`
-  ADD PRIMARY KEY (`bodenId`);
 -- Indexen voor tabel `tblcache`
 --
 ALTER TABLE `tblcache`
@@ -211,6 +227,12 @@ ALTER TABLE `tblgebruikers`
   ADD PRIMARY KEY (`gebruikerid`);
 
 --
+-- Indexen voor tabel `tblmessage`
+--
+ALTER TABLE `tblmessage`
+  ADD PRIMARY KEY (`messageid`);
+
+--
 -- Indexen voor tabel `tblproducten`
 --
 ALTER TABLE `tblproducten`
@@ -227,32 +249,28 @@ ALTER TABLE `tblrapporten`
 --
 
 --
--- AUTO_INCREMENT voor een tabel `tblboden`
---
-ALTER TABLE `tblboden`
-  MODIFY `bodenId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
--- AUTO_INCREMENT voor een tabel `tblcache`
---
-ALTER TABLE `tblcache`
-  MODIFY `cacheid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
 -- AUTO_INCREMENT voor een tabel `tblfacturen`
 --
 ALTER TABLE `tblfacturen`
-  MODIFY `factuurid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `factuurid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT voor een tabel `tblgebruikers`
 --
 ALTER TABLE `tblgebruikers`
-  MODIFY `gebruikerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `gebruikerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT voor een tabel `tblmessage`
+--
+ALTER TABLE `tblmessage`
+  MODIFY `messageid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT voor een tabel `tblproducten`
 --
 ALTER TABLE `tblproducten`
-  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT voor een tabel `tblrapporten`
