548e1c217518144e946781004f0bc9ca95831faa
CasperZP <casper.nauwelaerts@bazandpoort.be>
2024-02-05 16:24:49 +0100

    add reclame

--- src/index.php
+++ src/index.php
@@ -14,6 +14,14 @@ include "overzichtVeilingen.php";
 </head>
 
 <body class="h-screen" data-theme='<?php echo $_SESSION["theme"] ?>'>
-  
+
+<body>
+  <video width="320" height="240" controls autoplay src="src\reclame\HappyEgg.mp4">
+    <object data="movie.mp4" width="320" height="240">
+        <embed width="320" height="240" src="src\reclame\HappyEgg.mp4">
+    </object>
+  </video>
+</body>
+
 </body>
 </html>
