ac4a6f0413707861987c81b6139961bba21c6bc1
Ayranta <144694538+CasperZP@users.noreply.github.com>
2024-02-08 14:06:31 +0100

    fix foto

--- src/reclame.php
+++ src/reclame.php
@@ -1,3 +1,16 @@
+
+<?php
+include "connect.php";
+include "components/navbar.php";
+if (isset($_POST["submit"])) {
+  header("location:index.php");
+
+  
+
+
+
+};
+?>
 <!DOCTYPE html>
 <html lang="en" data-theme="<?php echo $_SESSION['theme'] ?>">
 <head>
@@ -9,19 +22,12 @@
 </head>
 <body class="h-screen" data-theme="<?php echo $_SESSION['theme'] ?>">
 	  <?php
-include "connect.php";
-include "components/navbar.php";
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+
 $_SESSION["theme"] = 'retro';
 
 $theme = ($change_theme['theme'] === 'dark') ? 'dark' : 'retro';
 $_SESSION["theme"] = $theme;
 
-if (isset($_POST["submit"])) {
-    header("location:index.php");
-};
 
 
 
@@ -30,15 +36,17 @@ if (isset($_POST["submit"])) {
 ?>
 <div class="h-screen flex items-center justify-center">
 <div class="card w-96 bg-base-100 shadow-xl ">
-  <figure class="px-10 pt-10 w-max h-max">
-    <img src="../public/img/ads.png" alt="ads" class="rounded-xl" >
+  <figure class="px-10 pt-10 w-max h-max ">
+    <img src="../public/img/adss.png" alt="ads" class="rounded-xl object-center" >
   </figure>
   <div class="card-body items-center text-center">
     <h2 class="card-title">Want ads?</h2>
-    <p>By buying this your products will be on top of the search results.</p>
+    <p>By buying this your products will be on top of the search results. And will never be gone.</p>
     <div class="card-actions">
-    <input type="submit" name="submit" value="Buy now" class="btn text-black bg-white mt-3 w-full border-white hover:text-white hover:bg-black"/>
-    </div>
+    <form method="post" action="reclame.php">
+    <button name="submit" class="btn text-black object-center bg-white mt-3 w-full border-white hover:text-white hover:bg-black">Buy now</button>
+    </form>  
+  </div>
   </div>
 </div>
 </div>
